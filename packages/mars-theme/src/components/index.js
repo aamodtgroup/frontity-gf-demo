88ad5c9310b4f13ed4096991c739327a0ffec877
Kasper Aamodt <kasper@aamodt.xyz>
2021-07-05 11:21:21 +0200

    Updated package

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -60,6 +60,20 @@ const globalStyles = css`
     color: inherit;
     text-decoration: none;
   }
+  .screen-reader-text {
+    border: 0;
+    clip: rect(1px,1px,1px,1px);
+    -webkit-clip-path: inset(50%);
+    clip-path: inset(50%);
+    height: 1px;
+    width: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute !important;
+    word-wrap: normal !important;
+    word-break: normal;
+  }
 `;
 
 const HeadContainer = styled.div`
